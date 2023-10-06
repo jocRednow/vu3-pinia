e6893c76154945b3ab8c80182f51c528e590df8b
stepankrasnov <s.krasnov@spacepass.pro>
2023-10-06 10:52:50 +0200

    added api

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About page</h1>
   </div>
 </template>
+
+<script setup lang="ts">
+  async function postJSON() {
+  try {
+    const response = await fetch("https://api.kinopoisk.dev/v1/studio", {
+      method: "GET",
+      headers: {
+        "X-API-KEY": "4Q2T31C-T4D4AM3-ME8QHQ0-F9W4CNT",
+      },
+    });
+
+    const result = await response.json();
+    console.log("Success:", result);
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+const data = postJSON();
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
