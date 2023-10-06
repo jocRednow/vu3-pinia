1cf1621b3493fa5903f1c67a567e38ecdd91d6ab
stepankrasnov <s.krasnov@spacepass.pro>
2023-10-06 11:29:52 +0200

    set api

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,26 +4,28 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+  let resolve = fetch("https://jsonplaceholder.typicode.com/todos/4");
+  let response = resolve.then((response) => response.json())
+                      .then((post) => console.log(post));
   async function postJSON() {
-  try {
-    const response = await fetch("https://api.kinopoisk.dev/v1/studio", {
-      method: "GET",
-      headers: {
-        "X-API-KEY": "4Q2T31C-T4D4AM3-ME8QHQ0-F9W4CNT",
-      },
-    });
+    try {
+      const response = await fetch("https://api.kinopoisk.dev/v1/studio", {
+        method: "GET",
+        headers: {
+          "X-API-KEY": "4Q2T31C-T4D4AM3-ME8QHQ0-F9W4CNT",
+        },
+      });
 
-    const result = await response.json();
-    console.log("Success:", result);
-    return result;
-  } catch (error) {
-    console.error("Error:", error);
+      const result = await response.json();
+      console.log(response);
+      console.log("Success:", result);
+      return result;
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
-}
-
-const data = postJSON();
-
+  const data = postJSON().then(res => { return res });
 </script>
 
 <style>
